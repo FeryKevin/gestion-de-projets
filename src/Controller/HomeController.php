2df7578a080c743d38936a72ab0c4b4cb304355b
FeryKevin <kevin.fery@outlook.com>
2022-11-21 16:25:07 +0100

    add front home

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route(path: "/", name: "home")]
+    public function home(Request $request): Response
+    {
+        return $this->render('home.html.twig', [
+        ]);
+    }
+
+}
